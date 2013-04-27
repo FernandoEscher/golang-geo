fb0fae3ed1c5b01a720358136b0af3c500433289
kellydunn <defaultstring@gmail.com>
2013-04-27 14:58:50 -0700

    [src] Adding some comments to the geocoder interface

--- geocoder.go
+++ geocoder.go
@@ -1,5 +1,9 @@
 package geo
 
+// The Geocoder Interface
+// This interfaces specifies the responsibilities of a Geocoder.
+// A Geocoder should be able to find a corresponding Point struct from a supplied  address.
+// A Geocoder should be able to find a corresponding address when supplied with a Point struct.
 type Geocoder interface {
 	Geocode(query string) (*Point, error)
 	ReverseGeocode(p *Point) (string, error)
